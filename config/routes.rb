e0bc01a35b52d1273079e4cccae605904bbfeea5
Francisco Rosa <chicorosa@gmail.com>
2020-03-28 22:21:59 +0000

    small route fix that bugged push to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home' as 'home'
+  root to: 'pages#home' as: 'home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts
   resouces :projects
