41b5458fc6fd6a58343264ae2bf460920a5a3e29
Francisco Rosa <chicorosa@gmail.com>
2020-03-28 22:25:51 +0000

    small route fix that bugged push to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home', as: 'home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts
-  resouces :projects
+  resources :projects
 end
