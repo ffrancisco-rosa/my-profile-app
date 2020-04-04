f8581335388b76fb7bd30a09db1f49bb28a9604e
Francisco Rosa <chicorosa@gmail.com>
2020-04-04 15:36:08 +0100

    project show almost done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :avatar
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
